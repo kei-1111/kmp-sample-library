06eb135b05e87a218737d4de3659eef0e44968c1
kei-1111 <kei_02_1111@icloud.com>
2025-07-14 15:41:46 +0900

    API を叩いたときに受け取るためのクラスを作成

---
+++ core/model/src/commonMain/kotlin/io/github/kei_1111/kmp_sample_library/core/model/MarsProperty.kt
@@ -0,0 +1,16 @@
+package io.github.kei_1111.kmp_sample_library.core.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MarsProperty(
+    val id: String,
+    val price: Int,
+    val type: PropertyType,
+    @SerialName("img_src")
+    val imgSrc: String
+)
+
+@Serializable
+enum class PropertyType { BUY, RENT }
