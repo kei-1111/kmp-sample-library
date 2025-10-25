97fdcdaa2896d69ea59e50adebdadf240b59108c
kei-1111 <kei_02_1111@icloud.com>
2025-10-25 16:15:51 +0900

    chore: MarsPropertyCardをクリックしたときのActionを定義

--- feature/home/src/commonMain/kotlin/io/github/kei_1111/kmp_sample_library/feature/home/HomeAction.kt
+++ feature/home/src/commonMain/kotlin/io/github/kei_1111/kmp_sample_library/feature/home/HomeAction.kt
@@ -1,6 +1,8 @@
 package io.github.kei_1111.kmp_sample_library.feature.home
 
 import io.github.kei_1111.kmp_sample_library.core.featurebase.Action
+import io.github.kei_1111.kmp_sample_library.core.model.MarsProperty
 
-interface HomeAction : Action {
+sealed interface HomeAction : Action {
+    data class OnClickMarsPropertyCard(val marsProperty: MarsProperty) : HomeAction
 }
