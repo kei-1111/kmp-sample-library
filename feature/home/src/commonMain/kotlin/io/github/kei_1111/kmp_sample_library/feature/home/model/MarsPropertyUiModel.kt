87ece2c5026dd31ed365fb0aef9b7711b8efa679
kei-1111 <kei_02_1111@icloud.com>
2025-10-26 17:40:05 +0900

    chore: MarsPropertyを表示する際に使うMarsPropertyUiModelを作成

---
+++ feature/home/src/commonMain/kotlin/io/github/kei_1111/kmp_sample_library/feature/home/model/MarsPropertyUiModel.kt
@@ -0,0 +1,25 @@
+package io.github.kei_1111.kmp_sample_library.feature.home.model
+
+import io.github.kei_1111.kmp_sample_library.core.model.MarsProperty
+import io.github.kei_1111.kmp_sample_library.core.model.PropertyType
+
+data class MarsPropertyUiModel(
+    val id: String,
+    val price: String,
+    val type: PropertyType,
+    val imageUrl: String,
+) {
+    companion object {
+        fun convert(marsProperty: MarsProperty): MarsPropertyUiModel {
+            val reversedPrice = marsProperty.price.toString().reversed()
+            val formattedPrice = reversedPrice.chunked(3).joinToString(",").reversed()
+
+            return MarsPropertyUiModel(
+                id = marsProperty.id,
+                price = "$$formattedPrice",
+                type = marsProperty.type,
+                imageUrl = marsProperty.imgSrc,
+            )
+        }
+    }
+}
