42f02832b0b8d5d88420029d3681019590a4b3a6
kei-1111 <kei_02_1111@icloud.com>
2025-10-27 13:57:54 +0900

    chore: build-logicで使用する拡張関数を定義

---
+++ build-logic/convention/src/main/kotlin/io/github/kei_1111/kmp_sample_library/ProjectExtensions.kt
@@ -0,0 +1,20 @@
+package io.github.kei_1111.kmp_sample_library
+
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.kotlin.dsl.getByType
+
+internal fun DependencyHandler.implementation(dependencyNotation: Any): Dependency? =
+    add("implementation", dependencyNotation)
+
+internal val Project.libs
+    get(): VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+internal fun VersionCatalog.versions(name: String): String =
+    this.findVersion(name).get().requiredVersion
+
+internal fun VersionCatalog.library(name: String) =
+    this.findLibrary(name).get().get()
