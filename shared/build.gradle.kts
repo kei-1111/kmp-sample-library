25e3f8673566340ee73e4de1668f3a4730711277
kei-1111 <kei_02_1111@icloud.com>
2025-10-27 23:31:39 +0900

    chore: 作成したKmpLibraryPluginを:sharedに適応

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,36 +1,8 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.gradle.api.publish.PublishingExtension
-
-group = "io.github.kei_1111.kmp_sample_library"
-version = libs.versions.library.get()
-
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.mavenPublish)
+    alias(libs.plugins.kmp.sample.library.kmp.library)
 }
 
 kotlin {
-    androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-        publishLibraryVariants("release")
-    }
-    
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "Shared"
-            isStatic = true
-        }
-    }
-    
     sourceSets {
         androidMain.dependencies {
             implementation(libs.koin.android)
@@ -41,35 +13,9 @@ kotlin {
             implementation(projects.data)
             implementation(projects.feature.home)
         }
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
-        }
     }
 }
 
 android {
     namespace = "io.github.kei_1111.kmp_sample_library.shared"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
-}
-
-afterEvaluate {
-    extensions.configure<PublishingExtension> {
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/kei-1111/kmp-sample-library")
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as String?
-                    password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.token") as String?
-                }
-            }
-        }
-    }
 }
