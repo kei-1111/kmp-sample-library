2e8a8a1e5678c92f84f26a964c2d961b98ceeba6
kei-1111 <kei_02_1111@icloud.com>
2025-10-27 23:30:04 +0900

    chore: 作成したKmpFeaturePluginを:feature:homeに適応

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -1,75 +1,21 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
-group = "io.github.kei_1111.kmp_sample_library"
-version = libs.versions.library.get()
-
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinSerialization)
-    alias(libs.plugins.mavenPublish)
+    alias(libs.plugins.kmp.sample.library.kmp.feature)
 }
 
 kotlin {
-    androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-        publishLibraryVariants("release")
-    }
-
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "Home"
-            isStatic = true
-        }
-    }
-
     sourceSets {
         commonMain.dependencies {
-            implementation(libs.kotlinx.serialization.json)
-            implementation(libs.koin.core)
+            api(projects.core.featurebase)
+            implementation(projects.core.domain)
+            implementation(projects.core.model)
+
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
-            api(projects.core.featurebase)
-            implementation(projects.core.model)
-            implementation(projects.core.domain)
-        }
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
+            implementation(libs.koin.core)
         }
     }
 }
 
 android {
     namespace = "io.github.kei_1111.kmp_sample_library.feature.home"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
-}
-
-afterEvaluate {
-    extensions.configure<PublishingExtension> {
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/kei-1111/kmp-sample-library")
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as String?
-                    password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.token") as String?
-                }
-            }
-        }
-    }
 }
