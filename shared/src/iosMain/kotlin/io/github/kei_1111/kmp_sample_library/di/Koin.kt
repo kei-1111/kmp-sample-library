b51a2ab81f38da8b48a0dff7bf5f8b7c4c01b960
kei-1111 <kei_02_1111@icloud.com>
2025-11-09 19:49:49 +0900

    chore: SwiftでDIするヘルパー関数を作成

--- shared/src/iosMain/kotlin/io/github/kei_1111/kmp_sample_library/di/Koin.kt
+++ shared/src/iosMain/kotlin/io/github/kei_1111/kmp_sample_library/di/Koin.kt
@@ -3,11 +3,28 @@ package io.github.kei_1111.kmp_sample_library.di
 import io.github.kei_1111.kmp_sample_library.core.network.di.networkModule
 import io.github.kei_1111.kmp_sample_library.data.di.dataModule
 import io.github.kei_1111.kmp_sample_library.feature.home.di.homeModule
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ObjCClass
+import kotlinx.cinterop.ObjCProtocol
+import kotlinx.cinterop.getOriginalKotlinClass
 import org.koin.core.Koin
+import org.koin.core.component.KoinComponent
 import org.koin.core.context.startKoin
 
 actual fun initKoin(appContext: Any?) {
     startKoin {
         modules(networkModule, dataModule, homeModule)
     }.koin
+}
+
+@OptIn(BetaInteropApi::class)
+@Suppress("unused")
+object KoinResolver : KoinComponent {
+    fun resolve(objCObject: Any): Any = when (objCObject) {
+        is ObjCProtocol -> getKoin().get(getOriginalKotlinClass(objCProtocol = objCObject)!!)
+
+        is ObjCClass -> getKoin().get(getOriginalKotlinClass(objCClass = objCObject)!!)
+
+        else -> throw Exception(message = "Unknown Object Type.")
+    }
 }
