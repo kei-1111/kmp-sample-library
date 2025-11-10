8a5dc1bb763b9a016dbd7f5b10f990dea05473cf
kei-1111 <kei_02_1111@icloud.com>
2025-11-10 10:21:15 +0900

    chore: iOS側からViewModelを使うことができるようにViewModelProviderを作成

--- shared/src/iosMain/kotlin/io/github/kei_1111/kmp_sample_library/di/Koin.kt
+++ shared/src/iosMain/kotlin/io/github/kei_1111/kmp_sample_library/di/Koin.kt
@@ -2,13 +2,10 @@ package io.github.kei_1111.kmp_sample_library.di
 
 import io.github.kei_1111.kmp_sample_library.core.network.di.networkModule
 import io.github.kei_1111.kmp_sample_library.data.di.dataModule
+import io.github.kei_1111.kmp_sample_library.feature.home.HomeViewModel
 import io.github.kei_1111.kmp_sample_library.feature.home.di.homeModule
-import kotlinx.cinterop.BetaInteropApi
-import kotlinx.cinterop.ObjCClass
-import kotlinx.cinterop.ObjCProtocol
-import kotlinx.cinterop.getOriginalKotlinClass
-import org.koin.core.Koin
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
 import org.koin.core.context.startKoin
 
 actual fun initKoin(appContext: Any?) {
@@ -17,14 +14,6 @@ actual fun initKoin(appContext: Any?) {
     }.koin
 }
 
-@OptIn(BetaInteropApi::class)
-@Suppress("unused")
-object KoinResolver : KoinComponent {
-    fun resolve(objCObject: Any): Any = when (objCObject) {
-        is ObjCProtocol -> getKoin().get(getOriginalKotlinClass(objCProtocol = objCObject)!!)
-
-        is ObjCClass -> getKoin().get(getOriginalKotlinClass(objCClass = objCObject)!!)
-
-        else -> throw Exception(message = "Unknown Object Type.")
-    }
+object ViewModelProvider: KoinComponent {
+    fun provideHomeViewModel(): HomeViewModel = get()
 }
