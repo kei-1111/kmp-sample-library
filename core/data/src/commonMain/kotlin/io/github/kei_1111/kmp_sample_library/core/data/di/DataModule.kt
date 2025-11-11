fc42b04a61135177c42692d9d6fa4e4b90baba2d
kei-1111 <kei_02_1111@icloud.com>
2025-11-11 11:53:23 +0900

    refactor: Koinの書き方を変更

--- core/data/src/commonMain/kotlin/io/github/kei_1111/kmp_sample_library/core/data/di/DataModule.kt
+++ core/data/src/commonMain/kotlin/io/github/kei_1111/kmp_sample_library/core/data/di/DataModule.kt
@@ -2,8 +2,10 @@ package io.github.kei_1111.kmp_sample_library.core.data.di
 
 import io.github.kei_1111.kmp_sample_library.core.data.MarsRepositoryImpl
 import io.github.kei_1111.kmp_sample_library.core.domain.data.MarsRepository
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val dataModule = module {
-    single<MarsRepository> { MarsRepositoryImpl(get()) }
+    singleOf(::MarsRepositoryImpl) bind MarsRepository::class
 }
--- core/network/src/commonMain/kotlin/io/github/kei_1111/kmp_sample_library/core/network/di/NetworkModule.kt
+++ core/network/src/commonMain/kotlin/io/github/kei_1111/kmp_sample_library/core/network/di/NetworkModule.kt
@@ -3,9 +3,11 @@ package io.github.kei_1111.kmp_sample_library.core.network.di
 import io.github.kei_1111.kmp_sample_library.core.domain.network.MarsApiService
 import io.github.kei_1111.kmp_sample_library.core.network.api.MarsApiServiceImpl
 import io.github.kei_1111.kmp_sample_library.core.network.client.HttpClientFactory
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val networkModule = module {
     single { HttpClientFactory.create() }
-    single<MarsApiService> { MarsApiServiceImpl(get()) }
+    singleOf(::MarsApiServiceImpl) bind MarsApiService::class
 }
